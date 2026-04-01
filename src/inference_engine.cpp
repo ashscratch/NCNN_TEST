1f150a2612c016eeb10075927098627e58d375fb
Eureka <sy14691@qq.com>
2026-04-01 23:01:48 +0800

    分装类

---
+++ src/inference_engine.cpp
@@ -0,0 +1,53 @@
+#include "inference_engine.h"
+#include <iostream>
+
+//没有返回值，为了防止出现异常
+InferenceEngine::InferenceEngine(){  
+}
+
+InferenceEngine::~InferenceEngine(){  
+    net_.clear();
+}
+
+bool InferenceEngine::LoadModel(const std::string& param_path, const std::string& bin_path){
+    if(net_.load_param(param_path.c_str()) != 0){
+        std::cerr << "加载param失败" << param_path << std::endl;
+        return false;
+    }
+     if(net_.load_model(bin_path.c_str()) != 0){
+        std::cerr << "加载bin失败" << bin_path << std::endl;
+        return false;
+    }
+    return true;
+}
+
+int InferenceEngine::Infer(const cv::Mat& frame, float& out_confidence){
+    if(frame.empty()){
+        std::cerr << "传入视频流为空" << std::endl;
+        return -1;
+    }
+
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(frame.data, ncnn::Mat::PIXEL_BGR, 
+        frame.cols, frame.rows, target_size, target_size);//frame.data uchar*指针  uchar->float
+    in.substract_mean_normalize(mean_vals, 0);
+
+    //每次调用函数都会生成提取器
+    ncnn::Extractor ex = net_.create_extractor(); //创建前向传播处理器
+    net_.opt.num_threads = 4;
+    ex.input("data", in); //将张量in绑定到输入节点data->来自param文件
+    ncnn::Mat out;
+    ex.extract("prob", out);
+
+    float* out_data = (float*) out.data; //提取结果并转化为浮点数
+    int max_index = 0;
+    float max_prob = 0.0f;
+    for(int i = 0; i < out.w; i++){
+        if(out_data[i] > max_prob){
+            max_prob = out_data[i];
+            max_index = i;
+        }
+    }
+
+    out_confidence = max_prob;
+    return max_index;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,23 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "net.h"
 #include "inference_engine.h"
 
 int main()
 {
-    ncnn::Net squeezenet;
-    squeezenet.opt.num_threads = 4;
-
-    //导入网络模型&检查是否成功
-    int param_status = squeezenet.load_param("../assets/squeezenet_v1.1.param");
-    int bin_status = squeezenet.load_model("../assets/squeezenet_v1.1.bin");
-    if(param_status != 0){
-        std::cerr << "错误：无法导入拓扑结构" << std::endl;
+    InferenceEngine engine;
+    if(!engine.LoadModel("../assets/squeezenet_v1.1.param", "../assets/squeezenet_v1.1.bin")){
         return -1;
     }
-    if(bin_status != 0){
-        std::cerr << "错误：无法导入权重数据" << std::endl;
-        return -1;
-    }
-    std::cout << "模型加载成功" << std::endl;
+    std::cout << "模型初始化成功" << std::endl;
 
-    //图像读取和预处理
-    cv::Mat img = cv::imread("../assets/test.jpg", cv::IMREAD_COLOR);
-    if(img.empty()){
-        std::cerr << "加载图片失败" << std::endl;
-        return -1;
-    }
+    cv::Mat test_frame = cv::imread("../assets/test.jpg", cv::IMREAD_COLOR);
+    float confidence = 0.f;
+    int index = engine.Infer(test_frame, confidence);
 
-    ncnn::Mat in = ncnn::Mat::from_pixels_resize(img.data, ncnn::Mat::PIXEL_BGR, img.cols, img.rows, 227,227);//img.data uchar*指针  uchar->float
-    const float mean_val[3] = {104.f, 117.f, 123.f}; //储存减去的·BGR均值
-    in.substract_mean_normalize(mean_val, 0); //减去均值，0代表不除以方差
-
-    ncnn::Extractor ex = squeezenet.create_extractor(); //创建前向传播处理器
-    ex.input("data", in); //将张量in绑定到输入节点data->来自param文件
-
-    ncnn::Mat out;
-    ex.extract("prob", out); 
-    float* out_data = (float*) out.data; //提取结果并转化为浮点数
-
-    int max_index = 0;
-    float max_prob = 0.0f;
-    for(int i = 0; i < out.w; i++){
-        if(out_data[i] > max_prob){
-            max_prob = out_data[i];
-            max_index = i;
-        }
-    }
 
     std::cout << "推理完成" << std::endl;
-    std::cout << "预测类别索引： " << max_index << std::endl;
-    std::cout << "置信度： " << max_prob << std::endl;
-
-
+    std::cout << "预测类别索引： " << index << std::endl;
+    std::cout << "置信度： " << confidence << std::endl;
 
     return 0;
 }
