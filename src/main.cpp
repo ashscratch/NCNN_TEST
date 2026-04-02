fc60ee276253daed647346a1f6546643a2418210
Eureka <sy14691@qq.com>
2026-04-02 17:18:17 +0800

    引入视频流

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 #include "inference_engine.h"
 
@@ -10,14 +11,59 @@ int main()
     }
     std::cout << "模型初始化成功" << std::endl;
 
-    cv::Mat test_frame = cv::imread("../assets/test.jpg", cv::IMREAD_COLOR);
-    float confidence = 0.f;
-    int index = engine.Infer(test_frame, confidence);
+    cv::VideoCapture cap("../assets/demo.mp4");
+    if(!cap.isOpened()){
+        std::cerr << "错误：视频流打开失败" << std::endl;
+        return -1;
+    }
 
+    cv::Mat frame;
+    float confidence =0.0f;
+    std::cout << "开始进行实时推理... " << std::endl << "按下ESC键可退出" << std::endl;
 
-    std::cout << "推理完成" << std::endl;
-    std::cout << "预测类别索引： " << index << std::endl;
-    std::cout << "置信度： " << confidence << std::endl;
+    //循环处理视频流
+    while(true){
+        //开始时间
+        auto start_time = std::chrono::high_resolution_clock::now();
+        
+        //抓取帧
+        cap >> frame;
+        if(frame.empty()){
+            std::cout << "视频流故障或已结束" << std::endl;
+            return -1;
+        }
+
+        //执行推理
+        int class_id = engine.Infer(frame, confidence);
+
+        //记录结束时间和耗时
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+        //计算FPS
+        float fps = 1000.f / (duration.count() + 1e-5);
+
+        //结果可视化（置信度大于0.6时）
+        if(confidence > 0.6f){
+            std::string label = "Class" + std::to_string(class_id) + "Conf" + std::to_string(confidence) .substr(0,4); //置信度取4位
+            cv::putText(frame, label, cv::Point(10, 30), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 0.8, cv::Scalar(0, 255, 0), 2); //放置标签
+        }
+
+        //标注FPS
+        std::string fps_label = "FPS: " + std::to_string(fps).substr(0, 4);
+        cv::putText(frame, fps_label, cv::Point(10, 60), cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(255, 0, 0), 2);
+
+        cv::imshow("NCNN Edge Deployment Demo", frame);
+
+        //等待并判断是否退出
+        while(cv::waitKey(1) == 27){
+            break;
+        }
+    }
+
+    //释放硬件&销毁窗口
+    cap.release();
+    cv::destroyAllWindows();
 
     return 0;
 }
